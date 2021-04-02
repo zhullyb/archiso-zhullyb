9aa65fe4d837b691f6b07907371167dcb877b800
zhullyb <zhullyb@outlook.com>
2021-04-02 18:31:43 +0800

    Add build.sh and set .gitignore

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env -S sudo bash -e
+if [[ -e build ]] ; then
+  echo "==> Cleaning out old builds..."
+  rm -rf build
+fi
+mkdir -p build/{work,out}
+
+echo "==> Building ISO..."
+mkarchiso -v -w ./build/work -o ./build/out . --debug
